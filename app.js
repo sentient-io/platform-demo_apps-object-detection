285c1c2be8fc6b5fb82fb1d3ed5d54cd4046a222
ZQ <zqthedesigner@gmail.com>
2020-12-26 18:19:15 +0800

    Add pre-loaded testing image

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-var apikey = "ENTER YOUR API KEY"
+// var apikey = "ENTER YOUR API KEY"
+var apikey = '08464A1E68F24199B495'; // zqthedesigner
+// var apikey = 'A76DE7145C5641549429'; // zhangqiao@sentient.com
